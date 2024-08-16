4d45e7b273aaf0a1558079ad9f647af3c47fac3e
Reva Mahto <reva.mahto@gmail.com>
2024-08-16 00:53:26 -0400

    hero component

---
+++ portfolio-site/src/common/ThemeContext.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(
+    () => localStorage.getItem("theme") || "light"
+  ); // default theme is whatever is alr being used OR light
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return(
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
--- portfolio-site/src/sections/Hero/Hero.jsx
+++ portfolio-site/src/sections/Hero/Hero.jsx
@@ -1,11 +1,61 @@
-import styles from './Hero.module.css'
+import styles from "./Hero.module.css";
+import heroImg from "../../assets/hero-img.png";
+import sunIcon from "../../assets/sun.svg";
+import CV from "../../assets/cv.pdf";
+import twitterLight from "../../assets/twitter-light.svg";
+import githubLight from "../../assets/github-light.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import twitterDark from "../../assets/twitter-dark.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+import { useTheme } from "../../common/ThemeContext";
+import moonIcon from "../../assets/moon.svg";
 
 function Hero() {
-  he
+  const {theme, toggleTheme} = useTheme();
+
+  const themeIcon = theme === "light" ? sunIcon : moonIcon;
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+
   return (
-    <section id="hero">Some other text hero</section>
-  )
-  he
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img className={styles.hero} src={heroImg} alt="Reva Profile Pic" />
+        <img src={themeIcon} 
+        alt="Theme mode icon" 
+        className={styles.colorMode} 
+        onClick={toggleTheme}/>
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Reva
+          <br />
+          Mahto
+        </h1>
+        <h2>Full-Stack Developer</h2>
+      </div>
+      <span>
+        <a href="https://twitter.com/" target="_blank">
+          <img src={twitterIcon} alt="Twitter Icon" />
+        </a>
+        <a href="https://github.com/r-oli-m" target="_blank">
+          <img src={githubIcon} alt="Github Icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/reva-mahto/" target="_blank">
+          <img src={linkedinIcon} alt="Linkedin Icon" />
+        </a>
+      </span>
+      <p className={styles.description}> 
+        Passion for developing modern and creative React web apps for all
+        purposes.
+      </p>
+      <a href={CV} download>
+        <button className="hover"> Resume </button>
+      </a>
+    </section>
+  );
 }
 
-export default  Hero
+export default Hero;
